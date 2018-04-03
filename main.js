e2948d85746ab1d3f46e4f57875065120c672b18
Sarfaraz <ssarfaraz1997@gmail.com>
2018-04-03 13:16:16 +0530

    modification in json file

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ function loadJSON(file,callback) {
   xhr.overrideMimeType("application/json");
   xhr.open("GET",file,true);
   xhr.onreadystatechange=function() {
-    if(xhr.readystate ===4 && xhr.status== "200"){
+    if(xhr.readyState ===4 && xhr.status =="200"){
       callback(xhr.responseText);
     }
   };
   xhr.send();
 }
 loadJSON("data.json",function(text) {
-  let data=json.parse(text);
+  let data=JSON.parse(text);
   console.log(data);
 })
